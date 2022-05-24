fdd6fa550427d001498c8da87bb576da05e8fe34
David Federman <david.federman@outlook.com>
2022-05-23 22:58:19 -0700

    Rewrite the Serial API layer

--- src/ZWave/Driver.cs
+++ src/ZWave/Driver.cs
@@ -1,23 +1,24 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Threading.Channels;
+using Microsoft.Extensions.Logging;
 using ZWave.CommandClasses;
 using ZWave.Serial;
 using ZWave.Serial.Commands;
 
 namespace ZWave;
 
-public sealed class Driver : IDisposable
+public sealed class Driver : IAsyncDisposable
 {
-    private record struct AwaitedCommand(CommandId CommandId, TaskCompletionSource<DataFrame> TaskCompletionSource);
+    private record struct AwaitedFrameResponse(CommandId CommandId, TaskCompletionSource<DataFrame> TaskCompletionSource);
 
     private record struct UnresolvedCallbackKey(CommandId CommandId, byte SessionId);
 
     private readonly ILogger _logger;
 
-    private readonly ZWaveSerialPortStream _stream;
+    private readonly ChannelWriter<DataFrameTransmission> _dataFrameSendChannelWriter;
 
-    private readonly ZWaveFrameListener _frameListener;
+    private readonly ZWaveSerialPortCoordinator _serialPortCoordinator;
 
-    private readonly CommandScheduler _commandScheduler;
+    private readonly Task _frameProcessingTask;
 
     // Lock anything related to session ids or callbacks
     private readonly object _callbackLock = new object();
@@ -26,18 +27,44 @@ public sealed class Driver : IDisposable
 
     private byte _lastSessionId = 0;
 
-    // Note that only one request -> response session can happen at a time.
-    private AwaitedCommand? _awaitedCommandResponse;
+    // INS12350 6.5.2 due to the simple nature of the simple acknowledge mechanism, only one REQ->RES session is allowed
+    private AwaitedFrameResponse? _awaitedFrameResponse;
 
     // Currenty this is the only non-callback request we wait on. If there are more, this should become a pattern.
     private TaskCompletionSource<SerialApiStartedRequest>? _serialApiStartedTaskCompletionSource;
 
-    private Driver(ILogger logger, ZWaveSerialPortStream stream)
+    private Driver(ILogger logger, string portName, CancellationToken cancellationToken)
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        _stream = stream ?? throw new ArgumentNullException(nameof(stream));
-        _frameListener = new ZWaveFrameListener(logger, stream, ProcessFrame);
-        _commandScheduler = new CommandScheduler(logger, stream, this);
+
+        if (string.IsNullOrEmpty(portName))
+        {
+            throw new ArgumentNullException(nameof(portName));
+        }
+
+        // We can assume the single reader/writer based on the implementation of the serial port coordinator and the single frame processing task
+        Channel<DataFrameTransmission> dataFrameSendChannel = Channel.CreateUnbounded<DataFrameTransmission>(new UnboundedChannelOptions { SingleReader = true, SingleWriter = false });
+
+        // We can assume a single reader based on the implementation of the serial port coordinator. The writer is the driver, which may be called by multiple callers.
+        Channel<DataFrame> dataFrameReceiveChannel = Channel.CreateUnbounded<DataFrame>(new UnboundedChannelOptions { SingleReader = true, SingleWriter = true });
+
+        _serialPortCoordinator = new ZWaveSerialPortCoordinator(logger, portName, dataFrameSendChannel.Reader, dataFrameReceiveChannel.Writer);
+
+        _dataFrameSendChannelWriter = dataFrameSendChannel.Writer;
+
+        // Start a task to asynchronously handle any data frames recieved.
+        // Note: Since we're starting out own task, we don't need to ConfigureAwait anywhere dowstream.
+        // TODO: Should we consider parallelism here? Perhaps configurable? If so, dataFrameReceiveChannel options needs adjustment
+        _frameProcessingTask = Task.Run(
+            async () =>
+            {
+                // Intentionally not passing a cancellation token as the serial port coordinator is responsible to completing the channel.
+                await foreach (DataFrame frame in dataFrameReceiveChannel.Reader.ReadAllAsync())
+                {
+                    ProcessDataFrame(frame);
+                }
+            });
+
         Controller = new Controller(logger, this);
     }
 
@@ -48,27 +75,119 @@ public sealed class Driver : IDisposable
         string portName,
         CancellationToken cancellationToken)
     {
-        var stream = new ZWaveSerialPortStream(logger, portName);
-        var driver = new Driver(logger, stream);
-        await driver.InitializeAsync(cancellationToken).ConfigureAwait(false);
+        cancellationToken.ThrowIfCancellationRequested();
 
+        var driver = new Driver(logger, portName, cancellationToken);
+        await driver.InitializeAsync(cancellationToken).ConfigureAwait(false);
         return driver;
     }
 
-    public void Dispose()
+    public async ValueTask DisposeAsync()
     {
-        _frameListener.Dispose();
-        _stream.Dispose();
+        _dataFrameSendChannelWriter.Complete();
+
+        await _serialPortCoordinator.DisposeAsync();
+
+        // Allow the processing task to drain. Disposal of the serial port coordinator should complete the channel.
+        await _frameProcessingTask;
+    }
+
+    private void ProcessDataFrame(DataFrame frame)
+    {
+        switch (frame.Type)
+        {
+            case DataFrameType.REQ:
+            {
+                // Try handling unsolicited requests first, then check if it's a callback we're waiting for.
+                switch (frame.CommandId)
+                {
+                    case CommandId.SerialApiStarted:
+                    {
+                        // Unblock the task. TODO: Log if null (we got it unexpectedly)
+                        if (_serialApiStartedTaskCompletionSource != null)
+                        {
+                            _serialApiStartedTaskCompletionSource.SetResult(SerialApiStartedRequest.Create(frame));
+                        }
+
+                        break;
+                    }
+                    case CommandId.ApplicationUpdate:
+                    {
+                        var applicationUpdateRequest = ApplicationUpdateRequest.Create(frame);
+                        if (applicationUpdateRequest.Event == ApplicationUpdateEvent.NodeInfoReceived)
+                        {
+                            var node = Controller.Nodes[applicationUpdateRequest.Generic.NodeId];
+                            node.NotifyNodeInfoReceived(applicationUpdateRequest);
+                        }
+
+                        break;
+                    }
+                    case CommandId.ApplicationCommandHandler:
+                    {
+                        var applicationCommandHandler = ApplicationCommandHandler.Create(frame);
+                        var node = Controller.Nodes[applicationCommandHandler.NodeId];
+                        var commandClassFrame = new CommandClassFrame(applicationCommandHandler.Payload);
+                        node.ProcessCommand(commandClassFrame);
+                        break;
+                    }
+                    default:
+                    {
+                        // This assumes the first command parameter is always the session id. If this is ever
+                        // found not to be the case, we'll need to add the callback index to the key.
+                        var callbackKey = new UnresolvedCallbackKey(frame.CommandId, frame.CommandParameters.Span[0]);
+                        lock (_callbackLock)
+                        {
+                            if (_unresolvedCallbacks.TryGetValue(callbackKey, out TaskCompletionSource<DataFrame>? tcs))
+                            {
+                                tcs.SetResult(frame);
+                                _unresolvedCallbacks.Remove(callbackKey);
+                                return;
+                            }
+                        }
+
+                        break;
+                    }
+                }
+
+                break;
+            }
+            case DataFrameType.RES:
+            {
+                if (_awaitedFrameResponse == null)
+                {
+                    // We weren't expecting a response. Just drop it
+                    // TODO: Log
+                    return;
+                }
+
+                if (_awaitedFrameResponse.Value.CommandId != frame.CommandId)
+                {
+                    // We weren't expecting this response. Just drop it
+                    // TODO: Log
+                    return;
+                }
+
+                // Ensure we null out the field before setting the result to avoid race conditions.
+                var tcs = _awaitedFrameResponse.Value.TaskCompletionSource;
+                _awaitedFrameResponse = null;
+                tcs.SetResult(frame);
+                break;
+            }
+            default:
+            {
+                // From INS12350 5.4.3
+                // A receiving end MUST ignore reserved Type values.
+                _logger.LogSerialApiDataFrameUnknownType(frame.Type);
+                break;
+            }
+        }
     }
 
     private async Task InitializeAsync(CancellationToken cancellationToken)
     {
         _logger.LogDriverInitializing();
 
-        // Perform initialization sequence (INS12350 6.1)
-        await _stream.WriteAsync(Frame.NAK.Data, cancellationToken).ConfigureAwait(false);
-
-        // TODO: Add retry logic.
+        // Perform initialization sequence (INS12350 6.1). Note: ZWaveSerialPortCoordinator sends the NAK upon opening the port.
         try
         {
             await SoftResetAsync(cancellationToken).ConfigureAwait(false);
@@ -129,140 +248,6 @@ public sealed class Driver : IDisposable
         }
     }
 
-    private void ProcessFrame(Frame frame)
-    {
-        switch (frame.Type)
-        {
-            case FrameType.ACK:
-            case FrameType.NAK:
-            case FrameType.CAN:
-            {
-                _commandScheduler.NotifyCommandDelivered(frame.Type);
-                break;
-            }
-            case FrameType.Data:
-            {
-                DataFrame dataFrame = frame.ToDataFrame();
-                ProcessDataFrame(dataFrame);
-                break;
-            }
-            default:
-            {
-                // Ignore anything we don't recognize.
-                // TODO: Log
-                break;
-            }
-        }
-    }
-
-    private void ProcessDataFrame(DataFrame frame)
-    {
-        // From INS12350 5.4.6
-        // Data frame MUST be considered invalid if it is received with an invalid checksum.
-        // A host or Z-Wave chip MUST return a NAK frame in response to an invalid Data frame.
-        if (!frame.IsChecksumValid())
-        {
-            _logger.LogSerialApiInvalidDataFrame(frame);
-
-            SendFrame(Frame.NAK);
-
-            // From INS12350 6.4.2
-            // If a host application detects an invalid checksum three times in a row when receiving data frames, the 
-            // host application SHOULD invoke a hard reset of the device. If a hard reset line is not available, a soft 
-            // reset indication SHOULD be issued for the device.
-            // TODO
-        }
-
-        switch (frame.Type)
-        {
-            case DataFrameType.REQ:
-            {
-                // Immediately send the ACK
-                SendFrame(Frame.ACK);
-
-                // Try handling unsolicited requests first, then check if it's a callback we're waiting for.
-                switch (frame.CommandId)
-                {
-                    case CommandId.SerialApiStarted:
-                    {
-                        // Unblock the task. TODO: Log if null (we got it unexpectedly)
-                        if (_serialApiStartedTaskCompletionSource != null)
-                        {
-                            _serialApiStartedTaskCompletionSource.SetResult(SerialApiStartedRequest.Create(frame));
-                        }
-
-                        break;
-                    }
-                    case CommandId.ApplicationUpdate:
-                    {
-                        var applicationUpdateRequest = ApplicationUpdateRequest.Create(frame);
-                        if (applicationUpdateRequest.Event == ApplicationUpdateEvent.NodeInfoReceived)
-                        {
-                            var node = Controller.Nodes[applicationUpdateRequest.Generic.NodeId];
-                            node.NotifyNodeInfoReceived(applicationUpdateRequest);
-                        }
-
-                        break;
-                    }
-                    case CommandId.ApplicationCommandHandler:
-                    {
-                        var applicationCommandHandler = ApplicationCommandHandler.Create(frame);
-                        var node = Controller.Nodes[applicationCommandHandler.NodeId];
-                        var commandClassFrame = new CommandClassFrame(applicationCommandHandler.Payload);
-                        node.ProcessCommand(commandClassFrame);
-                        break;
-                    }
-                    default:
-                    {
-                        // This assumes the first command parameter is always the session id. If this is ever
-                        // found not to be the case, we'll need to add the callback index to the key.
-                        var callbackKey = new UnresolvedCallbackKey(frame.CommandId, frame.CommandParameters.Span[0]);
-                        lock (_callbackLock)
-                        {
-                            if (_unresolvedCallbacks.TryGetValue(callbackKey, out TaskCompletionSource<DataFrame>? tcs))
-                            {
-                                tcs.SetResult(frame);
-                                _unresolvedCallbacks.Remove(callbackKey);
-                                return;
-                            }
-                        }
-
-                        break;
-                    }
-                }
-
-                break;
-            }
-            case DataFrameType.RES:
-            {
-                if (_awaitedCommandResponse == null)
-                {
-                    // We weren't expecting a response. Just drop it
-                    // TODO: Log
-                    return;
-                }
-
-                if (_awaitedCommandResponse.Value.CommandId != frame.CommandId)
-                {
-                    // We weren't expecting this response. Just drop it
-                    // TODO: Log
-                    return;
-                }
-
-                _awaitedCommandResponse.Value.TaskCompletionSource.SetResult(frame);
-                _awaitedCommandResponse = null;
-                break;
-            }
-            default:
-            {
-                // From INS12350 5.4.3
-                // A receiving end MUST ignore reserved Type values.
-                _logger.LogSerialApiDataFrameUnknownType(frame.Type);
-                break;
-            }
-        }
-    }
-
     internal byte GetNextSessionId()
     {
         lock (_callbackLock)
@@ -292,13 +277,6 @@ public sealed class Driver : IDisposable
             _lastSessionId = nextSessionId;
             return nextSessionId;
         }
-    }
-
-    internal Task<DataFrame> WaitForResponseAsync(CommandId commandId)
-    {
-        var tcs = new TaskCompletionSource<DataFrame>();
-        _awaitedCommandResponse = new AwaitedCommand(commandId, tcs);
-        return tcs.Task;
     }
 
     // TODO: This name is terrible. Consider doing more type magic for req/res vs callback flow
@@ -342,11 +320,13 @@ public sealed class Driver : IDisposable
         where TRequest : struct, ICommand<TRequest>
         where TResponse : struct, ICommand<TResponse>
     {
-        DataFrame responseFrame = await _commandScheduler.SendCommandAsync(
-            request.Frame,
-            expectResponse: true)
-            .WaitAsync(cancellationToken)
-            .ConfigureAwait(false);
+        var tcs = new TaskCompletionSource<DataFrame>();
+        _awaitedFrameResponse = new AwaitedFrameResponse(request.Frame.CommandId, tcs);
+
+        await SendFrameAsync(request.Frame, cancellationToken).ConfigureAwait(false);
+
+        DataFrame responseFrame = await tcs.Task.WaitAsync(cancellationToken);
+
         return TResponse.Create(responseFrame);
     }
 
@@ -354,11 +334,7 @@ public sealed class Driver : IDisposable
         TRequest request,
         CancellationToken cancellationToken)
         where TRequest : struct, ICommand<TRequest>
-    {
-        await _commandScheduler.SendCommandAsync(request.Frame)
-            .WaitAsync(cancellationToken)
-            .ConfigureAwait(false);
-    }
+        => await SendFrameAsync(request.Frame, cancellationToken).ConfigureAwait(false);
 
     internal async Task SendCommandAsync<TCommand>(
         TCommand request,
@@ -399,11 +375,14 @@ public sealed class Driver : IDisposable
         });
     }
 
-    private void SendFrame(Frame frame)
+    private async Task SendFrameAsync(DataFrame request, CancellationToken cancellationToken)
     {
-        // TODO: Make async? Frames are pretty small, so maybe not?
-        // TODO: Does this need to be coordinated with CommandScheduler?
-        _logger.LogSerialApiFrameSent(frame);
-        _stream.Write(frame.Data.Span);
+        var transmissionComplete = new TaskCompletionSource();
+        var transmission = new DataFrameTransmission(request, transmissionComplete);
+
+        await _dataFrameSendChannelWriter.WriteAsync(transmission, cancellationToken).ConfigureAwait(false);
+
+        // Wait until the frame is sent.
+        await transmissionComplete.Task;
     }
 }
